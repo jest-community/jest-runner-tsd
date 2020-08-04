2e080f9a96f5ce48a1a8cba78d1bd4403120e965
Karan Sanjeev <xalphamanx@tuta.io>
2020-08-04 19:25:25 +0530

    Added jest-docblock

--- src/run.js
+++ src/run.js
@@ -3,11 +3,26 @@
 const tsd = require('tsd');
 const { pass } = require('./pass');
 const { fail } = require('./fail');
+const { readFileSync } = require('fs');
+const { parse } = require('jest-docblock');
+
+/**
+ * @param {string} testPath 
+ * @returns {string}
+ */
+const findTypingsFile = testPath => {
+  const fileContents = readFileSync(testPath).toString();
+  const parsedDocblocks = parse(fileContents);
+  const typingsFile = String(parsedDocblocks.type);
+
+  return typingsFile;
+}
 
 module.exports = async ({ testPath }) => {
   // Convert absolute path to relative path
   const testFile = testPath.replace(process.cwd(), '');
   const start = +new Date();
+  const typingsFile = findTypingsFile(testPath);
   const extendedDiagnostics = await tsd.default({
     cwd: process.cwd(),
     testFiles: [testFile],
