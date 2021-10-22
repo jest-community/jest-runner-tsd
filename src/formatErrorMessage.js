44786ef2aa34a53606b5bb0de453039299573df6
mrazauskas <72159681+mrazauskas@users.noreply.github.com>
2021-10-22 10:01:12 +0300

    feat: pretty errors (#29)

---
+++ src/formatErrorMessage.js
@@ -0,0 +1,51 @@
+const { basename } = require('path');
+const { codeFrameColumns } = require('@babel/code-frame');
+const chalk = require('chalk');
+
+const BULLET = '\u25cf ';
+
+const TITLE_INDENT = '  ';
+const MESSAGE_INDENT = '    ';
+const CODE_INDENT = '    ';
+const LOCATION_INDENT = '      ';
+
+const NOT_EMPTY_LINE_REGEXP = /^(?!$)/gm;
+
+/**
+ * @param {string} lines
+ * @param {string} indent
+ */
+const indentAllLines = (lines, indent) =>
+  lines.replace(NOT_EMPTY_LINE_REGEXP, indent);
+
+module.exports = (diagnostics, fileContents) => {
+  const title = chalk.bold.red(TITLE_INDENT + BULLET + 'tsd typecheck') + '\n';
+
+  const messages = [];
+
+  diagnostics.forEach(error => {
+    const { column, fileName, line, message } = error;
+
+    const codeFrame = codeFrameColumns(
+      fileContents,
+      { start: { column: column + 1, line } },
+      { highlightCode: true, linesAbove: 0, linesBelow: 0 }
+    );
+
+    const location =
+      chalk.dim('at ') +
+      chalk.cyan(basename(fileName)) +
+      chalk.dim(':' + line + ':' + (column + 1));
+
+    messages.push(
+      indentAllLines(message, MESSAGE_INDENT) +
+        '\n\n' +
+        indentAllLines(codeFrame, CODE_INDENT) +
+        '\n\n' +
+        indentAllLines(location, LOCATION_INDENT) +
+        '\n'
+    );
+  });
+
+  return title + '\n' + messages.join('\n\n');
+};
