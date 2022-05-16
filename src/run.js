05c0626eb4903596a0f5024750e4899fed3735fc
Tom Mrazauskas <tom@mrazauskas.de>
2022-05-16 21:35:38 +0300

    feat: support `slowTestThreshold` option (#78)

--- src/run.js
+++ src/run.js
@@ -1,38 +1,81 @@
 const tsdLite = require('tsd-lite');
 const { formatTsdResults } = require('./formatter');
-const { fail } = require('./fail');
-const { pass } = require('./pass');
+
+/** @typedef {import('@jest/test-result').TestCaseResult} TestCaseResult */
 
 const TEST_TITLE = 'tsd typecheck';
 
+/**
+ * @param {'failed' | 'passed'} status
+ * @param {number} duration
+ * @param {string=} errorMessage
+ * @returns {TestCaseResult}
+ */
+function toJestResult(status, duration, errorMessage) {
+  return {
+    ancestorTitles: [],
+    failureDetails: [],
+    failureMessages: errorMessage ? [errorMessage] : [],
+    fullName: TEST_TITLE,
+    numPassingAsserts: status === 'passed' ? 1 : 0,
+    status,
+    title: TEST_TITLE,
+    duration,
+  };
+}
+
 /** @type {import('create-jest-runner').RunTest} */
-module.exports = ({ testPath }) => {
+module.exports = ({ testPath, config }) => {
   const start = Date.now();
 
   const { assertionsCount, tsdResults } = tsdLite.default(testPath);
 
   const end = Date.now();
 
-  const numFailed = tsdResults.length;
-  const numPassed = assertionsCount - numFailed;
+  const runtime = end - start;
+  const slow = runtime / 1000 > config.slowTestThreshold;
 
-  if (tsdResults.length > 0) {
-    const errorMessage = formatTsdResults(tsdResults);
+  const numFailingTests = tsdResults.length;
+  const numPassingTests = assertionsCount - numFailingTests;
 
-    return fail({
-      start,
-      end,
-      test: { path: testPath, title: TEST_TITLE },
-      numFailed,
-      numPassed,
-      errorMessage,
-    });
+  /** @type {string | null} */
+  let failureMessage = null;
+
+  /** @type {TestCaseResult | undefined} */
+  let testResult = undefined;
+
+  if (numFailingTests) {
+    failureMessage = formatTsdResults(tsdResults);
+    testResult = toJestResult('failed', runtime, failureMessage);
+  } else if (numPassingTests) {
+    testResult = toJestResult('passed', runtime);
   }
 
-  return pass({
-    start,
-    end,
-    test: { path: testPath, title: TEST_TITLE },
-    numPassed,
-  });
+  return {
+    failureMessage,
+    leaks: false,
+    numFailingTests,
+    numPassingTests,
+    numPendingTests: 0,
+    numTodoTests: 0,
+    openHandles: [],
+    perfStats: {
+      end,
+      runtime,
+      slow,
+      start,
+    },
+    skipped: false,
+    snapshot: {
+      added: 0,
+      fileDeleted: false,
+      matched: 0,
+      unchecked: 0,
+      uncheckedKeys: [],
+      unmatched: 0,
+      updated: 0,
+    },
+    testFilePath: testPath,
+    testResults: testResult ? [testResult] : [],
+  };
 };
